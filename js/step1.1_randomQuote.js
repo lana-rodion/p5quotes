8b9b712935b6dbc9811f41943bfa8379b70f3e62
lana-rodion <lana.rodion@gmail.com>
2020-03-23 14:09:47 +0100

    added icon and latest bootstrap link

--- js/step1.1_randomQuote.js
+++ js/step1.1_randomQuote.js
@@ -1,14 +1,16 @@
 // Version step1.1 2020/03/02 
-
-/* 
-Step 1.1 - Random quotes
+// Release step1.2 2020/03/21
+/*
+Step 1.2 - Random quotes
 
     Citations are generated randomly one by one each time the JavaScript program is called
     Each quote is the combination of at least 3 pieces of sentences
     Developer prepares sentence pieces
     The syntax of the sentence must be correct (no exclamation point in the middle of a sentence)
     The program can simply display the result in the JavaScript console
-
+    =====================================================
+    In JavaScript that is designed to be executed in the browser, it's considered a best practice to avoid using methods on console.
+    Calls using console should be stripped before being pushed to production.
 */
 
 "use strict";
@@ -51,16 +53,16 @@ function getCapitalizeFirstLetter(str) {
 // Create quote text from data
 function generatorRandomQuote() {
 	// return random text from data
-	var start = data.quotesStart[Math.floor(Math.random() * data.quotesStart.length)];
-	var center = data.quotesCenter[Math.floor(Math.random() * data.quotesCenter.length)];
-	var finish = data.quotesFinish[Math.floor(Math.random() * data.quotesFinish.length)];
+	let start = data.quotesStart[Math.floor(Math.random() * data.quotesStart.length)];
+	let center = data.quotesCenter[Math.floor(Math.random() * data.quotesCenter.length)];
+	let finish = data.quotesFinish[Math.floor(Math.random() * data.quotesFinish.length)];
 
-	var phrase = getCapitalizeFirstLetter(start) + " " + center + " " + finish + ".";
+	let phrase = getCapitalizeFirstLetter(start) + " " + center + " " + finish + ".";
 	console.log(phrase);
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
-	// call function when page is opened
+	// call function when page is loaded
 	generatorRandomQuote();
 
 });
